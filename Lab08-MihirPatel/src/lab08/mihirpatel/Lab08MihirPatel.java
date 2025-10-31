a5d42724d953f669b906c15bfb8a929fcb1647e0
Mihir Patel <mihusunny@gmail.com>
2025-10-31 17:29:47 -0400

    Add files via upload
    Finished the layout for the ui.

--- Lab08-MihirPatel/src/lab08/mihirpatel/Lab08MihirPatel.java
+++ Lab08-MihirPatel/src/lab08/mihirpatel/Lab08MihirPatel.java
@@ -4,17 +4,34 @@
  */
 package lab08.mihirpatel;
 
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
 /**
  *
  * @author 6316704
  */
-public class Lab08MihirPatel {
-
+public class Lab08MihirPatel extends Application{
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        launch(args);
     }
     
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        
+            Parent root = FXMLLoader.load(getClass().getResource("Lab08FXML.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        
+    }
 }
---
+++ Lab08-MihirPatel/src/lab08/mihirpatel/PainterController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab08.mihirpatel;
+
+/**
+ *
+ * @author Mihir
+ */
+public class PainterController {
+    
+}
